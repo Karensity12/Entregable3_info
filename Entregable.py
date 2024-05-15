c1966f020b8af0b3e62f8b1fe5d3fc8e435d7f54
alejandroloturco <alejandro.loturco@udea.edu.co>
2024-05-15 17:41:13 -0500

    3/4

--- Entregable.py
+++ Entregable.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 import pydicom
 
-def C_celulas(ruta):
+def contar(ruta):
     img = cv2.imread(ruta)
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_noise = cv2.GaussianBlur(img_gray, (5, 5), 0) #Reducir el ruido, (5,5)---> tamaño del kernel
@@ -28,6 +28,11 @@ def C_celulas(ruta):
 
     #Encontrar contornos (externos)
     contours, _ = cv2.findContours(imgC, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    """cv2.imshow('aqui',imgC)
+    cv2.waitKey(0)                  
+    cv2.destroyAllWindows()"""
+
 
     min_area = 20  
     max_area = 31  
@@ -39,25 +44,68 @@ def C_celulas(ruta):
 
     cell_count = len(filtered_contours)
     print(f"Number of cells: {cell_count}")
+    return cell_count
 
-#Pruebita :)
-ruta = r'C:\Users\Karen\Desktop\Imagenes info\Imagen celula.jpg'  
-C_celulas(ruta)
-
-
-
-def cargar_dicom(ruta):
-    list= []
+def cargar_dc(ruta):
+    dicoms= []
     archivos = os.listdir(ruta)
     archivos_dicom = sorted([archivo for archivo in archivos if archivo.endswith('.dcm')])
     for archivo in archivos_dicom:
-        ruta_c = os.path.join(ruta, archivo)
-        ds = pydicom.dcmread(ruta_c) #Objeto tipo DICOM
-        list.append(ds)
-    return list
+        ruta_dicoms = os.path.join(ruta, archivo)
+        ds = pydicom.dcmread(ruta_dicoms) 
+        dicoms.append(ds)
+    print("Archivos cargados")
+    return dicoms
 
 
-ruta = r'C:\Users\Karen\Desktop\Imagenes info\archivosDCM'
-imagenes_dicom = cargar_dicom(ruta)
+def mostrar_ds(dicoms):
+    fig = plt.figure()
+    repeticiones = int(input("Ingrese la cantidad de veces que desea ver las imagenes: ")) 
+    for i in range(repeticiones):
+        for x in dicoms:
+            plt.imshow(x.pixel_array, cmap = plt.cm.gray)
+            plt.title("imagen dicom")
+            plt.axis('off')
+            plt.pause(0.05)
+            plt.clf()
+        for x in dicoms[::-1]:
+            plt.imshow(x.pixel_array, cmap = plt.cm.gray)
+            plt.title("Imagen dicom")
+            plt.axis('off')
+            plt.pause(0.05)
+            plt.clf()
+    plt.close()
+    
+        
+
+def main():
+    x = ''
+    while True:
+        print("""Opciones
+            \r1-Contar celulas 
+            \r2-Cargar archivos dicom✅
+            \r3-Mostrar archivos dicom✅
+            \r4-Salir✅""")
+        opcion = int(input("Ingrese la opcion deseada: "))
+        if opcion == 1:
+            ruta = input("Ingrese la ruta de la imagen con las celulas a contar: ") 
+            contar(ruta)
+            continue
+        elif opcion == 2:
+            ruta = input("Ingrese la ruta de la carpeta con los archivos dicom: ")
+            x = cargar_dc(ruta)
+            continue
+        elif opcion == 3:
+            if x == '':
+                print("Aun no ha ingresado archivos, porfavor dirijase a la opcion 2")
+                continue
+            else:
+                mostrar_ds(x)
+                continue
+        elif opcion == 4:
+            print("Ha terminado")
+            break
+
+main()
 
 
