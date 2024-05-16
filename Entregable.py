62f116361b145ed567e04f1b713f6a4b8d955da4
alejandroloturco <alejandro.loturco@udea.edu.co>
2024-05-15 22:11:18 -0500

    Co-authored-by: Karensity12 <Karensity12@users.noreply.github.com>

--- Entregable.py
+++ Entregable.py
@@ -4,36 +4,16 @@ import numpy as np
 import os
 import pydicom
 
+
 def contar(ruta):
     img = cv2.imread(ruta)
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img_noise = cv2.GaussianBlur(img_gray, (5, 5), 0) #Reducir el ruido, (5,5)---> tamaño del kernel
-
-    
-    #Prueba 
-    cv2.namedWindow('Ventana', 0)   #Crear ventana
-    cv2.imshow('Ventana',img_noise)       #Mostrar la imagen en la ventana creada
-    cv2.waitKey(0)                  #Dejar la ventana abierta hasta el siguiente enter
-    cv2.destroyAllWindows()   
-
-    #Binarización
+    img_noise = cv2.GaussianBlur(img_gray, (5, 5), 0) 
     _, image_bin = cv2.threshold(img_noise, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-    #Operaciones morfologicas
     kernel = np.ones((3, 3), np.uint8)
-    imgOp = cv2.morphologyEx(image_bin, cv2.MORPH_OPEN, kernel, iterations=1) #Apertura: erosion + dolatacion 
-
-    #Closing
+    imgOp = cv2.morphologyEx(image_bin, cv2.MORPH_OPEN, kernel, iterations=1) 
     imgC = cv2.morphologyEx(imgOp, cv2.MORPH_CLOSE, kernel, iterations=1)
-
-    #Encontrar contornos (externos)
-    contours, _ = cv2.findContours(imgC, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
-    """cv2.imshow('aqui',imgC)
-    cv2.waitKey(0)                  
-    cv2.destroyAllWindows()"""
-
-
+    contours, _ = cv2.findContours(imgC, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)   
     min_area = 20  
     max_area = 31  
     filtered_contours = []
@@ -41,9 +21,8 @@ def contar(ruta):
         area = cv2.contourArea(cnt)
         if min_area < area < max_area:
             filtered_contours.append(cnt)
-
     cell_count = len(filtered_contours)
-    print(f"Number of cells: {cell_count}")
+    print(f"Numero de celulas: {cell_count}")
     return cell_count
 
 def cargar_dc(ruta):
@@ -75,8 +54,8 @@ def mostrar_ds(dicoms):
             plt.pause(0.05)
             plt.clf()
     plt.close()
-    
-        
+
+
 
 def main():
     x = ''
